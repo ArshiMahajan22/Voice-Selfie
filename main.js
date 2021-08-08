faa1d3d9f3a8f37ee70bd5ee0f437f059c7ee92e
ArshiMahajan22 <77196068+ArshiMahajan22@users.noreply.github.com>
2021-08-08 10:54:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+//Microphone
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start(){
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event){
+    console.log(event);
+    var speech = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML = speech;
+    console.log(speech);
+    if(speech == "take my selfie"){
+        console.log("Taking Selfie -----");
+        speak();
+    }
+}
+
+function speak(){
+    var Talk = window.speechSynthesis;
+    //Text-To-Speech-Converter
+    var textbox_text = "Taking Your Selfie in 5 seconds";
+    //Computer-Speaks
+    var utterThis = new SpeechSynthesisUtterance(textbox_text);
+    Talk.speak(utterThis);
+    Webcam.attach(camera);
+    setTimeout(function (){
+        take_Snapshot();
+        Save();
+    }, 5000);
+}
+
+//Camera
+Webcam.set({
+    width: 375,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
+
+function take_Snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("Selfie").innerHTML = '<img id="Selfie_IMG" src="'+data_uri+'"/>';
+    });
+}
+
+//Downloading the Image
+function Save(){
+    Selfie = document.getElementById("link");
+    Selfie_img = document.getElementById("Selfie_IMG").src;
+    Selfie.href = Selfie_img;
+    Selfie.click();
+}
